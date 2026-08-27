7784651f1e1abddb1444b82e121d210b76333cba
sruxti <ksrushti2005@gmail.com>
2026-08-27 13:13:58 +0530

    Delete Assignment3 directory

--- Assignment3/Que2.py
+++
@@ -1,6 +0,0 @@
-ch = (input("Enter alphabate: "))
-
-if ch == "a" or ch == "e" or ch == "i" or ch == "o" or ch == "u" or ch == "A" or ch == "E" or ch == "I" or ch == "O" or ch == "U":
-    print("IT is Vowel.")
-else:
-    print("Consonent.")
--- Assignment3/Que7.py
+++
@@ -1,9 +0,0 @@
-# WAP TO CHECK IF USER HAS ENTERED CORRECT USERID AND PASSWORD.
-
-userid = input("Enter User ID: ")
-password = input("Enter Password: ")
-
-if userid == "srushti" and password == "2005":
-    print("Login Successful")
-else:
-    print("Invalid User ID or Password")
